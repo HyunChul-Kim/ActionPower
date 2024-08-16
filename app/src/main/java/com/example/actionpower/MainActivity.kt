aa91342b1072c1757191ca8a4f21fdb960f8c24d
chul <guscjf4929@gmail.com>
2024-08-16 22:12:55 +0900

    기본 앱 화면 분리

--- app/src/main/java/com/example/actionpower/MainActivity.kt
+++ app/src/main/java/com/example/actionpower/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.actionpower.ui.SuperPowerApp
 import com.example.actionpower.ui.theme.ActionPowerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ActionPowerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperPowerApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ActionPowerTheme {
-        Greeting("Android")
     }
 }
