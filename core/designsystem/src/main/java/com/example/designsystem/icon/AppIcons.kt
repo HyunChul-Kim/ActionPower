3e665aba568d23f38e08cd4e9c99ec80c8a0632a
chul <guscjf4929@gmail.com>
2024-08-21 21:19:58 +0900

    - 검색어 취소 버튼 추가 - 검색어 취소 기능 추가

--- core/designsystem/src/main/java/com/example/designsystem/icon/AppIcons.kt
+++ core/designsystem/src/main/java/com/example/designsystem/icon/AppIcons.kt
@@ -3,8 +3,10 @@ package com.example.designsystem.icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.StarBorder
+import androidx.compose.material.icons.rounded.Close
 
 object AppIcons {
     val Favorite = Icons.Filled.Star
     val FavoriteBorder = Icons.Outlined.StarBorder
+    val Close = Icons.Rounded.Close
 }
--- feature/search/src/main/java/com/example/search/component/SearchBar.kt
+++ feature/search/src/main/java/com/example/search/component/SearchBar.kt
@@ -8,15 +8,19 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.designsystem.icon.AppIcons
 
 @Composable
 fun SearchBar(
@@ -52,6 +56,20 @@ private fun SearchTextField(
                 contentDescription = "Search"
             )
         },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                        onSearchQueryChanged("")
+                    },
+                ) {
+                    Icon(
+                        imageVector = AppIcons.Close,
+                        contentDescription = "Clear text",
+                    )
+                }
+            }
+        },
         value = searchQuery,
         onValueChange = onSearchQueryChanged,
         keyboardOptions = KeyboardOptions(
@@ -68,6 +86,15 @@ private fun SearchTextField(
 private fun SearchBarPreview() {
     SearchBar(
         query = "",
+        onSearchQueryChanged = {}
+    )
+}
+
+@Preview
+@Composable
+private fun SearchBarPreviewExistText() {
+    SearchBar(
+        query = "query",
         onSearchQueryChanged = {}
     )
 }
